8824b982b67c895c52d21e0674a9bee785e92e06
Maxamed459 <maxamedmahdi459@gmail.com>
2025-07-16 22:02:59 +0300

    add changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router";
 import { AuthProvider } from "../context/AuthContext";
-import Landing from "./pages/Landing";
 import Header from "./components/Header";
 
 const App = () => {
